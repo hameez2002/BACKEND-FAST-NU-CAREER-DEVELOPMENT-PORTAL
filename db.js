8a13f14ccc76a6dc18338d7b5a5968a1c518b0f0
Hameez Ahmed <hameezahmed23@gmail.com>
2023-12-26 19:47:04 +0500

    updated database fro deployment

--- db.js
+++ db.js
@@ -2,10 +2,13 @@ const mysql = require("mysql");
 
 const con = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "backend",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  waitForConnections: true,
+  queueLimit:0
 });
 
+console.log(process.env.DB_HOST);
 module.exports = con;
