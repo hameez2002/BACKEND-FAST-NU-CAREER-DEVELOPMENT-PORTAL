0b74f9c53b074195be48495e909040a239c533a8
Hameez Ahmed <hameezahmed23@gmail.com>
2023-12-27 01:04:33 +0500

    connecting backend with frontend api

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 const con = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DBNAME || "backend",
   waitForConnections: true,
   queueLimit:0
 });
