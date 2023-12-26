38d6fa51749c17aaf70256897960e2927f46a42f
Hameez Ahmed <hameezahmed23@gmail.com>
2023-12-26 16:28:50 +0500

    Added env file

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const jobsGet = require("./Routes/jobsGet.js");
 const jobsDelete = require("./Routes/jobsDelete.js");
 const jobsEdit = require("./Routes/jobsEdit.js");
 const partialJobsEdit = require("./Routes/partialJobsEdit.js");
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 7000;
+const port = process.env.PORT ;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
@@ -29,7 +30,7 @@ const multer = require("multer");
 const uploadMiddleware = multer({ dest: "uploads/" });
 const fs = require("fs");
 app.use("/uploads", express.static(__dirname + "/uploads"));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_API }));
 app.use(express.json());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
@@ -38,8 +39,8 @@ mongoose.set("strictQuery", false);
 //   // "mongodb://hameezahmed23:nMbGO9kRfXZ1xKje@main-shard-00-00-03xkr.mongodb.net:27017,main-shard-00-01-03xkr.mongodb.net:27017,main-shard-00-02-03xkr.mongodb.net:27017/main?ssl=true&replicaSet=Main-shard-0&authSource=admin&retryWrites=true"
 //   "mongodb://hameezahmed23:hameez12@cluster0.zrq5jo8.mongodb.net/?retryWrites=true&w=majority"
 // );
-mongoose.connect('mongodb+srv://hameezahmed23:nMbGO9kRfXZ1xKje@cluster0.zrq5jo8.mongodb.net/?retryWrites=true&w=majority');
-
+mongoose.connect(process.env.MONGODB_CONNECT_URI);
+// console.log(process.env.MONGODB_CONNECT_URI);
 app.post("/newsfeed/createPost", uploadMiddleware.single("file"), async (req, res) => {
   console.log('Hellopost');
   const { originalname, path } = req.file;
