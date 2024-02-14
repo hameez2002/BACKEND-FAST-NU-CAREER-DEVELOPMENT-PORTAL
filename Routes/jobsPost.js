cc2184fe2edc20419990e1aaee20b62910cff033
Shayan <shayan.saleem24@gmail.com>
2024-02-14 15:50:07 +0500

    job fields integrated

--- Routes/jobsPost.js
+++ Routes/jobsPost.js
@@ -4,14 +4,15 @@ module.exports = (req, res) => {
   console.log("Received request body:", req.body);
   const {
     jobTitle: title,
-    noofopeinings: no_of_openings,
-    qualificationsReq: qualification_req,
+    noOfOpenings:no_of_openings,
+    qualificationRequirements: qualification_req,
     jobDescription: job_description,
-    Responsibilities: responsibilities,
-    About: about,
+    responsibilities,
+    about,
     jobStatus: job_status,
     jobType: job_type
   } = req.body;
+
   const sql =
     "INSERT INTO jobs (title, job_type, no_of_openings, qualification_req, job_description, responsibilities, about, job_status) VALUES (?, ?, ?, ?, ?,?,?,?)";
   con.query(sql, [title, job_type, no_of_openings, qualification_req, job_description, responsibilities, about, job_status], (err, result) => {
