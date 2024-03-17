b5ca7d9c117f777dbbab0bc09606d83bc6c9d010
Hameez Ahmed <hameezahmed23@gmail.com>
2024-03-17 15:15:26 +0500

    Profile resolve final # 1

--- knexFile.js
+++ knexFile.js
@@ -16,7 +16,10 @@ const db = require("./db"); // Import the MySQL connection pool from db.js
 const knex = require("knex")({
   client: "mysql",
   connection: {
-    connection: db,
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_DBNAME || "backend",
   },
 });
 
