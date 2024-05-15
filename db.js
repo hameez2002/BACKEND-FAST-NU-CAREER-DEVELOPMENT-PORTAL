493f3be270f0db106b0c4966319e42d8aedc9f0e
Hameez Ahmed <hameezahmed23@gmail.com>
2024-05-16 03:54:41 +0500

    Final deployment 3

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 const con = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DBNAME || "backend",
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_DBNAME ,
   waitForConnections: true,
   queueLimit:0
   // host: "localhost",
